2a67a02157334e6a2a34c64c9c2f44d4a7ccca73
Dimitar Vashkov <dimitarvashkov@outlook.com>
2021-11-02 20:16:00 +0200

    Created template driven form for add-property

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.scss']
 })
 export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm: NgForm;
+
+
 
   constructor(private router: Router) { }
 
@@ -15,5 +19,9 @@ export class AddPropertyComponent implements OnInit {
 
   onBack(): void {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log(this.addPropertyForm);
   }
 }
